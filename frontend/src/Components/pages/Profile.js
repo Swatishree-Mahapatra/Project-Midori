22bbf3451be8a947d57b2b72db3c8a25d666f322
Abhas-15 <abhirup.abhas@gmail.com>
2021-12-18 17:08:21 +0530

    style:profile style improved

--- frontend/src/Components/pages/Profile.js
+++ frontend/src/Components/pages/Profile.js
@@ -2,11 +2,11 @@ import React from "react";
 import dp from "../assets/testimonial_img-1.jpg";
 const Profile = () => {
   return (
-    <div className="h-screen bg-pink-100">
+    <div className="h-screen ">
       {/* <div>
         <h1 className="p-12 font-medium text-2xl ">Wants to Plant More</h1>
       </div> */}
-      <div className="flex md:flex-row flex-col justify-around ">
+      <div className="flex md:flex-row bg-green-500 flex-col justify-around ">
         <div className="h-full w-1/2">
           <h1 className="bg-blue-100 text-center py-2 font-medium text-2xl my-8 ml-45">
             WELCOME!
@@ -26,8 +26,8 @@ const Profile = () => {
             
           </div>
         </div>
-        <div className="h-screen w-1/2 bg-white p-20 text-center">
-          <h2 className="text-4xl text-center pb-2 bg-white -ml-20">
+        <div className="h-screen w-1/2 bg-yellow-100 p-20 text-center">
+          <h2 className="text-4xl text-center pb-2 bg-brown ">
             UPLOAD HERE
           </h2>{" "}
           <div className="pt-6">
@@ -41,12 +41,9 @@ const Profile = () => {
           </div>
           <div className="pt-6">
             <label htmlFor="img">Upload your Image here.</label>
-            <br />
-            <input
-              className={
-                "w-3/4 bg-blue-100 p-20 text-primary  transition duration-150 ease-in-out mb-4"
-              }
-            />
+            <br/><br/>
+            
+            <input className="ml-20" type="file"></input>
           </div>
           <button class="py-2 m-8 px-4 bg-pink-100 text-black font-semibold rounded-lg shadow-md hover:bg-white focus:outline-none focus:ring-2 focus:ring-white-400 focus:ring-opacity-75">
             Submit
