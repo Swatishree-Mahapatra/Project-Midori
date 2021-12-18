2c819b57b6a7a35ee4bfd3d18244fc77916e27d7
swatishree1508 <swatishree1508@gmail.com>
2021-12-18 18:48:01 +0530

    backend initial commit

---
+++ backend/app/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    points:{
+      type:Number,
+      required: true,
+    }
+  })
+);
+
+module.exports = User;
